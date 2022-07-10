cec7cc2a026da9706dfb57e0a27f0606aac8fa75
ichiKD <94745178+ichiKD@users.noreply.github.com>
2022-07-10 16:23:28 +0530

    Add files via upload

--- Codeforces/RockandLever.c
+++ Codeforces/RockandLever.c
@@ -4,29 +4,23 @@ int main(){
 	scanf("%d",&testcases);
 	int n;
 	int arr[100400];
-	int mostSignificantBit[32]={0};
-	int sum, t;
+	long long int mostSignificantBit[40]={0};
+	long long int sum;
 	while(testcases--){
 		scanf("%d",&n);
-		for(int i=0;i<32;i++){
-			mostSignificantBit[i]=0;
-		}
-		t=0;
+		for(int i=0;i<32;i++)	mostSignificantBit[i]=0;
 		for(int i=0;i<n;i++) scanf("%d",&arr[i]);
-		for(int i=0;i<n;i++){
-			scanf("%d",&arr[i]);
-			while(arr[i]!=0){
-				arr[i]>>1;
-				t++;
-				printf("%d",arr[i]);
+		
+		for(int j=0;j<32;j++){
+			for(int i=0;i<n;i++){
+				if(arr[i]>=(1<<j)&&arr[i]<(1<<j+1)) mostSignificantBit[j]++;
 			}
-			mostSignificantBit[t]++;
 		}
 		sum=0;
-		for(int i=0;i<26;i++){
-			sum+=mostSignificantBit[i]*mostSignificantBit[i]/2;
+		for(int i=0;i<30;i++){
+			sum+=mostSignificantBit[i]*(mostSignificantBit[i]-1)/2;
 		}
-		printf("%d\n",sum);
+		printf("%lld\n",sum);
 	}
 	
 	return 0;
